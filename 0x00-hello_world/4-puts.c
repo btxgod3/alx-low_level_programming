24f87bebbfe7828a286719787beae16434e7f00b
Malvin <brook-mts@outlook.com>
2022-06-30 07:07:18 -0700

    First commit of the day!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the statement "Programming is like buidling a multilingual puzzle"
+ * Return : 0
+ */
+int main(){
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
