44ee991d02d4d520f515bd3a2b79be37386abbf1
Malvin <brook-mts@outlook.com>
2022-06-30 08:51:41 -0700

    Next

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - This program prints exactly "with proper grammar, but"
  * " the outcome is a piece of art,"
  *
- * Return :Always 0 (sucess)
+ * Return: Always 0 (sucess)
  */
 
 int main(void)
