54948d4c2eeb4bf8ab4a28bdb34b742d42734ed8
Malvin <brook-mts@outlook.com>
2022-07-05 07:15:58 -0700

    Check

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main -  check the code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++
+	}
+	_putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
